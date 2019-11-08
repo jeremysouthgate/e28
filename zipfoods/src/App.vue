d39afad4958b0fb49c5e9792efe810e5c50ccec3
Jeremy C. Southgate <jeremycsouthgate@Jeremys-Mac-mini.local>
2019-11-07 23:20:08 -0500

    Progress on zipfoods after Week 9, Part 4

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,26 +1,39 @@
 <template>
     <div id="app">
         <img id='logo' alt="Zipfoods logo" src="./assets/images/zipfoods-logo.png">
-        <p>
-            “ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.”
-        </p>
-        <show-featured :products='products'></show-featured>
-        <show-products :products='products'></show-products>
+        <nav>
+            <ul>
+                <li v-for='link in links' :key='link'>
+                    <a href='#' @click='page = link'>{{ link }}</a>
+                </li>
+            </ul>
+        </nav>
+        <component :is='linkComponents[page]'></component>
     </div>
 </template>
 
 <script>
+import ShowHome from './components/ShowHome.vue';
+import ShowCategories from './components/ShowCategories.vue';
 import ShowProducts from './components/ShowProducts.vue';
-import ShowFeatured from './components/ShowFeatured.vue';
 import {products} from './products.js';
+
 export default {
   name: 'app',
   components: {
+      ShowHome,
       ShowProducts,
-      ShowFeatured
+      ShowCategories
   },
   data: function() {
       return {
+          page: 'home',
+          links: ['home', 'products', 'categories'],
+          linkComponents: {
+              home: 'ShowHome',
+              products: 'ShowProducts',
+              categories: 'ShowCategories'
+          },
           products: products
       }
   }

324528d9469e5a987d0b33a9fee069a483261b41
Jeremy C. Southgate <jeremycsouthgate@Jeremys-Mac-mini.local>
2019-11-07 22:59:21 -0500

    Progress on zipfoods after Week 9, Part 3

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -4,13 +4,26 @@
         <p>
             “ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.”
         </p>
+        <show-featured :products='products'></show-featured>
+        <show-products :products='products'></show-products>
     </div>
 </template>
 
 <script>
+import ShowProducts from './components/ShowProducts.vue';
+import ShowFeatured from './components/ShowFeatured.vue';
+import {products} from './products.js';
 export default {
   name: 'app',
-  components: {}
+  components: {
+      ShowProducts,
+      ShowFeatured
+  },
+  data: function() {
+      return {
+          products: products
+      }
+  }
 }
 </script>
 
---
+++ zipfoods/src/components/ShowProduct.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class='product'>
+        <div class='product-name'>{{ product.name }}</div>
+        <img
+            :alt='"Product Image of" + product.name'
+            :src='"../assets/images/products/" + product.id + ".jpg"'
+        />
+        <p class='product-description'>{{ product.description }}</p>
+        <div class='product-price'>{{ product.price }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ShowProduct",
+    props: ['product']
+}
+</script>
+
+<style scoped>
+
+</style>
